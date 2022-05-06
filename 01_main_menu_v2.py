1f59cebb354e8a51c09b41fbbc9184a7e9068b29
ummmh <eugenioj2@middleton.school.nz>
2022-05-06 14:16:33 +1200

    Create 01_main_menu_v2.py

---
+++ 01_main_menu_v2.py
@@ -0,0 +1,75 @@
+"""Component 1 of Te Reo Māori Quiz
+Second version of the Main menu GUI - buttons now print what quiz was selected
+Created by Janna Lei Eugenio
+6/05/2022
+"""
+
+from tkinter import *
+import random
+
+
+# Main menu
+class Main:
+    def __init__(self, parent):
+        # Formatting variables
+        background_colour = "white"
+
+        # Main menu Screen GUI
+        self.main_frame = Frame(bg=background_colour, padx=10, pady=10)
+        self.main_frame.grid()
+
+        # Te Reo Māori Quiz Heading (row 0)
+        self.main_heading = Label(self.main_frame, text="Te Reo Māori Quiz",
+                                  font="Helvetica 16 bold",
+                                  bg=background_colour)
+        self.main_heading.grid(row=0)
+
+        # Introduction (row 1) - filler text for now
+        self.main_intro_label = Label(self.main_frame, text="(introduction)",
+                                      font="Helvetica 10", pady=5,
+                                      bg=background_colour)
+        self.main_intro_label.grid(row=1)
+
+        # Quiz select button frame (row 2)
+        self.quiz_select_buttons_frame = Frame(self.main_frame, pady=10,
+                                               bg=background_colour)
+        self.quiz_select_buttons_frame.grid(row=2)
+
+        # Colours quiz button (row 2, column 0)
+        self.colours_button = Button(self.quiz_select_buttons_frame,
+                                     text="Colours", font="Helvetica 14",
+                                     bg="#F8CECC",  # light red
+                                     command=self.select_colours)
+        self.colours_button.grid(row=0, column=0)
+
+        # Numbers quiz button (row 2, column 1)
+        self.numbers_button = Button(self.quiz_select_buttons_frame,
+                                     text="Numbers 1-10", font="Helvetica 14",
+                                     bg="#F5F5F5",  # off white
+                                     command=self.select_numbers)
+        self.numbers_button.grid(row=0, column=1)
+
+        # Days of the Week quiz button (row 2, column 2)
+        self.days_button = Button(self.quiz_select_buttons_frame,
+                                  text="Days of the Week", font="Helvetica 14",
+                                  bg="#BAC8D3",  # light grey
+                                  command=self.select_days)
+        self.days_button.grid(row=0, column=2)
+
+    def select_colours(self):
+        print("colours")
+
+    def select_numbers(self):
+        print("numbers")
+
+    def select_days(self):
+        print("days")
+
+
+# MAIN ROUTINE
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Te Reo Māori Quiz")
+    something = Main(root)
+    root.mainloop()
+
