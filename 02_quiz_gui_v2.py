173d67d4d18f0641a06c34a50d6b0a2a272e0789
ummmh <eugenioj2@middleton.school.nz>
2022-05-09 10:05:09 +1200

    Update 02_quiz_gui_v2.py

--- 02_quiz_gui_v2.py
+++ 02_quiz_gui_v2.py
@@ -40,22 +40,31 @@ class Main:
         self.colours_button = Button(self.quiz_select_buttons_frame,
                                      text="Colours", font="Helvetica 14",
                                      bg="#F8CECC",  # light red
-                                     command=Colours)
+                                     command=self.select_colours)
         self.colours_button.grid(row=0, column=0)
 
         # Numbers quiz button (row 2, column 1)
         self.numbers_button = Button(self.quiz_select_buttons_frame,
                                      text="Numbers 1-10", font="Helvetica 14",
                                      bg="#F5F5F5",  # off white
-                                     command=Numbers)
+                                     command=self.select_numbers)
         self.numbers_button.grid(row=0, column=1)
 
         # Days of the Week quiz button (row 2, column 2)
         self.days_button = Button(self.quiz_select_buttons_frame,
                                   text="Days of the Week", font="Helvetica 14",
                                   bg="#BAC8D3",  # light grey
-                                  command=Days)
+                                  command=self.select_days)
         self.days_button.grid(row=0, column=2)
+
+    def select_colours(self):
+        Colours(self)
+
+    def select_numbers(self):
+        Numbers(self)
+
+    def select_days(self):
+        Days(self)
 
 
 class Colours:
