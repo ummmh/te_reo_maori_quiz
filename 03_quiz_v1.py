d38f09da4202f725f5388ac92ffdb8440c1f3046
ummmh <eugenioj2@middleton.school.nz>
2022-05-16 09:39:57 +1200

    Update 03_quiz_v1.py

--- 03_quiz_v1.py
+++ 03_quiz_v1.py
@@ -1,5 +1,36 @@
 """Component 3 of Te Reo Māori Quiz
-Main quiz
+Main quiz function (only colours for now for testing)
 Created by Janna Lei Eugenio
 13/05/2022
 """
+
+import random  # to select a random colour from the list
+
+# list containing the questions and answers
+COLOURS = [["Mā", "white"], ["Whero", "red"], ["Kākāriki", "green"],
+           ["Mangu", "black"], ["Pango", "black"], ["Kōwhai", "yellow"],
+           ["Parauri", "brown"], ["Kikorangi", "blue"], ["Karaka", "orange"],
+           ["Waiporoporo", "purple"], ["Kiwikiwi", "grey"]]
+total = len(COLOURS)  # total number of questions
+
+# count up the amount of correct answers
+correct = 0
+
+# main routine
+while COLOURS:
+    # Asks the question
+    question = random.choice(COLOURS)  # select a random colour from list
+    print(question[0])
+    COLOURS.remove(question)
+
+    # User inputs an answer
+    answer = input().lower()
+    if answer == question[1]:
+        print("correct\n")
+        correct += 1
+    else:
+        print("incorrect")
+        print(f"the correct number was {question[1]}\n")
+
+# Score output
+print(f"\nScore: {correct}/{total}")
