4495004ebb020a4bc9fc1003a499e606f8f0607e
ummmh <eugenioj2@middleton.school.nz>
2022-05-19 14:04:48 +1200

    Create 03_quiz_v3.py

---
+++ 03_quiz_v3.py
@@ -0,0 +1,65 @@
+"""Component 3 of Te Reo Māori Quiz
+third version - uses dictionaries instead of lists
+Created by Janna Lei Eugenio
+18/05/2022
+"""
+
+import random  # to select a random word from the list
+
+
+# quiz function
+def quiz(selection):
+
+    colours = {"Mā": "white", "Whero": "red", "Kākāriki": "green",
+               "Mangu / Pango": "black", "Kōwhai": "yellow",
+               "Parauri": "brown", "Kikorangi": "blue", "Karaka": "orange",
+               "Waiporoporo": "purple", "Kiwikiwi": "grey"}
+
+    numbers = {'Tahi': "one" "1", 'Rua': "two" "2", 'Toru': "three" "3",
+               'Whā': "four" "4", 'Rima': "five" "5", 'Ono': "six" "6",
+               'Whitu': "seven" "7", 'Waru': "eight" "8", 'Iwa': "nine" "9",
+               'Tekau': "ten" "10"}
+
+    days = {'Rāhina': "monday", 'Rātū': "tuesday", 'Rāapa': "wednesday",
+            'Rāpare': "thursday", 'Rāmere': "friday", 'Rāhoroi': "saturday",
+            'Rātapu': "sunday"}
+
+    # list containing the questions and answers
+    questions = {}
+
+    if selection == "C":
+        questions = colours
+    elif selection == "N":
+        questions = numbers
+    elif selection == "D":
+        questions = days
+
+    # total number of questions
+    total = len(questions)
+
+    # count up the amount of correct answers
+    correct_answers = 0
+
+    # ask user questions
+    while questions:
+        # Asks the question
+        question = random.choice(list(questions))  # select a random colour from list
+        print(question)
+
+        # User inputs an answer
+        answer = input().lower()
+        if answer == question:
+            print("correct\n")
+            correct_answers += 1
+        else:
+            print("incorrect")
+            print(f"the correct number was {question[1]}\n")
+
+    # Score output
+    print(f"Score: {correct_answers}/{total}")
+
+
+# main
+# quiz input for testing - later will be based on the button on main GUI
+quiz_selection = input("enter quiz selection: ").upper()
+quiz(quiz_selection)
