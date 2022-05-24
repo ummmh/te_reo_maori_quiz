d5a0b64d6436db93b1558773cf91f8da801e2da7
ummmh <eugenioj2@middleton.school.nz>
2022-05-24 12:03:50 +1200

    Update 04_quiz_gui_v7.py

--- 04_quiz_gui_v7.py
+++ 04_quiz_gui_v7.py
@@ -1,5 +1,6 @@
 """Component 4 of Te Reo Māori Quiz
-Incorporating 03_quiz_v2 with 02_quiz_gui_v6 - formatting the quiz GUI
+Incorporating 03_quiz_v2 with 02_quiz_gui_v6 - added a start quiz button to
+each quiz window that will later be connected to the quiz function
 Created by Janna Lei Eugenio
 20/05/2022
 """
@@ -219,65 +220,6 @@ class Days(tk.Frame):
 
     def start_quiz(self):
         print("start")
-
-
-# quiz function
-def quiz(selection):
-    # separate lists relating to quiz selection
-    colours = [["Mā", "white"], ["Whero", "red"], ["Kākāriki", "green"],
-               ["Mangu", "black"], ["Pango", "black"], ["Kōwhai", "yellow"],
-               ["Parauri", "brown"], ["Kikorangi", "blue"],
-               ["Karaka", "orange"], ["Waiporoporo", "purple"],
-               ["Kiwikiwi", "grey"]]
-
-    numbers = [["Tahi", "one", "1"], ["Rua", "two", "2"],
-               ["Toru", "three", "3"], ["Whā", "four", "4"],
-               ["Rima", "five", "5"], ["Ono", "six", "6"],
-               ["Whitu", "seven", "7"], ["Waru", "eight", "8"],
-               ["Iwa", "nine", "9"], ["Tekau", "ten", "10"]]
-
-    days = [["Rāhina", "monday"], ["Rātū", "tuesday"], ["Rāapa", "wednesday"],
-            ["Rāpare", "thursday"], ["Rāmere", "friday"],
-            ["Rāhoroi", "saturday"], ["Rātapu", "sunday"]]
-
-    # list containing the questions and answers
-    questions = []
-
-    if selection == "C":
-        questions = colours
-    elif selection == "N":
-        questions = numbers
-    elif selection == "D":
-        questions = days
-
-    # total number of questions
-    total = len(questions)
-
-    # count up the amount of correct answers
-    correct_answers = 0
-
-    # ask user questions
-    while questions:
-        # Asks the question
-        question = random.choice(questions)  # select a random colour from list
-        print(question[0])
-        questions.remove(question)
-
-        # User inputs an answer
-        answer = input().lower()
-        try:
-            if answer == question[1] or answer == question[2]:
-                print("correct\n")
-                correct_answers += 1
-            else:
-                print("incorrect")
-                print(f"the correct answer was {question[1]}\n")
-        except IndexError:
-            print("incorrect")
-            print(f"the correct answer was {question[1]}\n")
-
-    # Score output
-    print(f"Score: {correct_answers}/{total}")
 
 
 # MAIN ROUTINE
